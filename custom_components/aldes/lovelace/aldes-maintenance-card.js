df4eb1b0c7f68fd18f0157c54e59201c76e52959
saniho <sanihogithub@gmail.com>
2026-05-11 22:50:21 +0200

    feat: restore editor file and registration

--- custom_components/aldes/lovelace/aldes-maintenance-card.js
+++ custom_components/aldes/lovelace/aldes-maintenance-card.js
@@ -4,40 +4,6 @@ import {
   css,
 } from "https://unpkg.com/lit-element@2.4.0/lit-element.js?module";
 
-class AldesMaintenanceCardEditor extends LitElement {
-  static get properties() {
-    return { hass: {}, config: {} };
-  }
-
-  setConfig(config) {
-    this.config = config;
-  }
-
-  render() {
-    if (!this.hass || !this.config) return html``;
-
-    return html`
-      <div class="card-config">
-        <ha-entity-picker
-          .hass="${this.hass}"
-          .value="${this.config.modem_entity}"
-          .configValue="${'modem_entity'}"
-          @value-changed="${this._valueChanged}"
-          label="Modem Entity"
-          allow-custom-entity
-        ></ha-entity-picker>
-      </div>
-    `;
-  }
-
-  _valueChanged(ev) {
-    const config = { ...this.config, [ev.target.configValue]: ev.detail.value };
-    this.config = config;
-    this.dispatchEvent(new CustomEvent("config-changed", { detail: { config } }));
-  }
-}
-customElements.define("aldes-maintenance-card-editor", AldesMaintenanceCardEditor);
-
 class AldesMaintenanceCard extends LitElement {
   static get properties() {
     return {
